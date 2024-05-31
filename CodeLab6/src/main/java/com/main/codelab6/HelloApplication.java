eaae8835341408f5b54d95990c7eefb724b8c167
irfandeny <irfandeny112@gmail.com>
2024-06-01 06:23:39 +0700

    mod6.1

--- CodeLab6/src/main/java/com/main/codelab6/HelloApplication.java
+++ CodeLab6/src/main/java/com/main/codelab6/HelloApplication.java
@@ -19,50 +19,41 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Create the login form
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        // Add title
         Label title = new Label("Halaman Login");
         title.setFont(Font.font("Times new roman", FontWeight.BOLD, 24));
         grid.add(title, 0, 0, 2, 1);
 
-        // Add username label and field
-        Label userName = new Label("User Name:");
+        Label userName = new Label("User Name\t:");
         grid.add(userName, 0, 1);
         TextField userField = new TextField();
             grid.add(userField, 1, 1);
 
-            // Add password label and field
-            Label pw = new Label("Password:");
+            Label pw = new Label("Password\t\t:");
             grid.add(pw, 0, 2);
             PasswordField pwField = new PasswordField();
             grid.add(pwField, 1, 2);
 
-            // Add login button
             Button btn = new Button("Sign in");
             HBox hbBtn = new HBox(10);
             hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
             hbBtn.getChildren().add(btn);
             grid.add(hbBtn, 1, 4);
 
-            // Add error message
             Label errorLabel = new Label("");
             errorLabel.setTextFill(Color.RED);
             grid.add(errorLabel, 1, 3);
 
-            // Handle login button click
             btn.setOnAction(event -> {
                 String username = userField.getText();
             String password = pwField.getText();
 
-            // Check if username and password are correct
             if (username.equals("fanden") && password.equals("377")) {
-                // Create the success scene
                 GridPane successGrid = new GridPane();
                 successGrid.setAlignment(Pos.CENTER);
                 successGrid.setHgap(10);
@@ -78,17 +69,15 @@ public class HelloApplication extends Application {
                 successGrid.add(hbOk, 0,1 );
 
                 okButton.setOnAction(e -> primaryStage.close());
-                Scene successScene = new Scene(successGrid, 300, 200);
+                Scene successScene = new Scene(successGrid, 320, 250);
                 primaryStage.setScene(successScene);
                 primaryStage.show();
             } else {
-                // Show error message
                 errorLabel.setText("Password Atau Username Salah");
             }
         });
 
-        // Create the login scene
-        Scene scene = new Scene(grid, 300, 275);
+        Scene scene = new Scene(grid, 320, 250);
         primaryStage.setTitle("Form Login");
         primaryStage.setScene(scene);
         primaryStage.show();
